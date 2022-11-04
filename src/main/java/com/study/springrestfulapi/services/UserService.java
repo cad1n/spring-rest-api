d77f3dea0ab67056e12640d967845866919590aa
Caio Augusto <caioaugusto4520@gmail.com>
2022-11-03 22:08:07 -0300

    Adding project structure and MongoDB config

---
+++ src/main/java/com/study/springrestfulapi/services/UserService.java
@@ -0,0 +1,19 @@
+package com.study.springrestfulapi.services;
+
+import com.study.springrestfulapi.domain.User;
+import com.study.springrestfulapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}

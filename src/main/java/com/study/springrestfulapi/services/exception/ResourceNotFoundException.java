311a70af7dbecf08c05091222372ca6e013cb7ee
Caio Augusto <caioaugusto4520@gmail.com>
2023-06-19 18:15:10 -0300

    Adding @Serial annotation on serialUID declarations

--- src/main/java/com/study/springrestfulapi/services/exception/ResourceNotFoundException.java
+++ src/main/java/com/study/springrestfulapi/services/exception/ResourceNotFoundException.java
@@ -1,6 +1,9 @@
 package com.study.springrestfulapi.services.exception;
 
+import java.io.Serial;
+
 public class ResourceNotFoundException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(String msg) {
